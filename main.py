1bfe1a09da54f77530e093bd6b97f2d17ff2266d
Alex Leoniuk <alex.brest.by@gmail.com>
2023-07-24 21:18:58 +0300

    fix: critical security bug

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import os
 import logging
 import requests
 from bs4 import BeautifulSoup
 from telegram import Update
+from dotenv import load_dotenv
 from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler
+
+load_dotenv()
+token = os.getenv("BELMETA_TOKEN")
 
 def strongCheck(titles):
   for strongList in titles:
@@ -48,7 +53,7 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=response_array)
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('6551954937:AAH8m-cZyWYxNmc65YIRa7tsT1IxnfYUAHo').build()
+    application = ApplicationBuilder().token(token).build()
     
     start_handler = CommandHandler('start', start)
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
