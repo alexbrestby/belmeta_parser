887fa54915e2409973f056a68548a624c3fcf47c
Alex Leoniuk <alex.brest.by@gmail.com>
2024-03-13 08:53:15 +0300

    fix: hh soup crash fix

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTyp
 load_dotenv()
 token = os.getenv("BELMETA_TOKEN")
 
-def strongCheck(titles):
-  for strongList in titles:
-    if strongList.find("strong") is not None:
-      title = strongList.find("strong")
-      print(type(title.text))
-      # return title.text
+
+def strong_check(titles):
+    for strongList in titles:
+        if strongList.find("strong") is not None:
+            title = strongList.find("strong")
+            print(type(title.text))
+            # return title.text
+
 
 # profession = str(input("Какую вакансию будем искать?\n"))
 # pg_count = int(input("Сколько страниц надо обработать?\n"))
@@ -24,62 +26,67 @@ logging.basicConfig(
     level=logging.INFO
 )
 
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Введите название вакансии, которую ищете...")
 
+
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    count = 0
     profession = update.message.text
-    response_array_belmeta = ""     
-    response_array_hh = ""     
+    response_array_belmeta = ""
+    response_array_hh = ""
+    count = 0
+
     await context.bot.send_message(chat_id=update.effective_chat.id, text="минуточку...")
-    for i in range(0,20):
+    for i in range(0, 20):
 
-      url_belmeta = f"https://belmeta.com/vacansii?l=Брест&sort=date&page={i}"
-      url_hh = f"https://hh.ru/search/vacancy?area=1007&order_by=publication_time&page={i}"
-      
-      response_belmeta = requests.get(url_belmeta, headers={'User-Agent': 'Mozilla/5.0'})
-      response_hh = requests.get(url_hh, headers={'User-Agent': 'Mozilla/5.0'})
+        url_belmeta = f'https://belmeta.com/vacansii?l=Брест&sort=date&page={i}'
+        url_hh = f"https://hh.ru/search/vacancy?area=1007&order_by=publication_time&page={i}"
 
-      soup_belmeta = BeautifulSoup(response_belmeta.content, "html.parser")
-      soup_hh = BeautifulSoup(response_hh.content, "html.parser")
+        response_belmeta = requests.get(url_belmeta, headers={'User-Agent': 'Mozilla/5.0'})
+        response_hh = requests.get(url_hh, headers={'User-Agent': 'Mozilla/5.0'})
 
-      titles_belmeta = soup_belmeta.select(".title a")
-      titles_hh = soup_hh.select(".serp-item__title")
+        soup_belmeta = BeautifulSoup(response_belmeta.content, "html.parser")
+        soup_hh = BeautifulSoup(response_hh.content, "html.parser")
 
-      company_belmeta = soup_belmeta.select(".company")
-      company_hh = soup_hh.select(".bloko-link_kind-tertiary")
-      print(profession)
-      # strongCheck(titles)
+        titles_belmeta = soup_belmeta.select(".title a")
+        titles_hh = soup_hh.select(".serp-item__title-link")
 
-      for j in range(len(titles_belmeta)):
-        if (titles_belmeta[j].text.lower().__contains__(profession.lower())):
-            count+=1
-            response_array_belmeta += f"{count} - {titles_belmeta[j].text}\n{company_belmeta[j].text}\nhttps://belmeta.com{titles_belmeta[j]['href']}\n\n"
+        company_belmeta = soup_belmeta.select(".company")
+        company_hh = soup_hh.select(".vacancy-serp-item__meta-info-company")
 
-      for k in range(len(titles_hh)):
-        if (titles_hh[k].text.lower().__contains__(profession.lower())):
-            count+=1
-            response_array_hh += f"{count} - {titles_hh[k].text}\n{company_hh[k].text}\n{titles_hh[k]['href']}\n\n"
+        link_hh = soup_hh.select(".serp-item__title-link-wrapper")
+        print(profession)
+        # strongCheck(titles)
+
+        for j in range(len(titles_belmeta)):
+            if titles_belmeta[j].text.lower().__contains__(profession.lower()):
+                count += 1
+                response_array_belmeta += \
+                    (f"{count} - {titles_belmeta[j].text}\n{company_belmeta[j].text}\n"
+                     f"https://belmeta.com{titles_belmeta[j]['href']}\n\n")
+
+        for k in range(len(titles_hh)):
+            if titles_hh[k].text.lower().__contains__(profession.lower()):
+                count += 1
+                response_array_hh += f"{count} - {titles_hh[k].text}\n{company_hh[k].contents[0].text}\n{link_hh[k].contents[0]['href'].split('?')[0]}\n\n"
 
     if response_array_belmeta == "":
-      response_array_belmeta = "На Белмете ничего не найдено"
+        response_array_belmeta = "На Белмете ничего не найдено"
 
     if response_array_hh == "":
-      response_array_hh = "На Хедхантере ничего не найдено"
+        response_array_hh = "На Хедхантере ничего не найдено"
 
     await context.bot.send_message(chat_id=update.effective_chat.id, text=response_array_hh)
     await context.bot.send_message(chat_id=update.effective_chat.id, text=response_array_belmeta)
 
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token(token).build()
-    
+
     start_handler = CommandHandler('start', start)
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
-    
+
     application.add_handler(start_handler)
     application.add_handler(echo_handler)
     application.run_polling()
-
-
-
